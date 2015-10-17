d84fc890c0df95bb9f11ef6e546304d5cc5637a5
Dustin Carlton <dtc0004@uah.edu>
2015-10-17 01:25:37 -0500

    Compilation now uses multiprocessing

--- PokemonEmblem.bat
+++ PokemonEmblem.bat
@@ -1,6 +1,7 @@
 @echo off
 
 pushd build
-cl /EHsc /ZI /W4 /WX /wd4996 ..\Main.cpp ..\src\Gameplay\*.cpp ..\src\GUI\*.cpp ..\src\Utility\*.cpp ..\src\State\*.cpp /I ..\include /link /libpath ..\lib\SDL2.lib
+cl /EHsc /ZI /MP /W4 /WX /wd4996 ..\Main.cpp ..\src\Gameplay\*.cpp ..\src\GUI\*.cpp ..\src\Utility\*.cpp ..\src\State\*.cpp /I ..\include /link /libpath ..\lib\SDL2.lib
 Main.exe
+del Main.exe
 popd
--- RunTests.bat
+++ RunTests.bat
@@ -1,7 +1,7 @@
 @echo off
 
 pushd build
-cl /EHsc /ZI /W4 /WX /wd4996 ..\Tests.cpp ..\src\Utility\*.cpp ..\test\UnitTests\Utility\*.cpp ..\src\State\*.cpp ..\test\UnitTests\State\*.cpp ..\src\Data\*.cpp /I ..\include /link /libpath ..\lib\SDL2.lib
+cl /EHsc /ZI /MP /W4 /WX /wd4996 ..\Tests.cpp ..\src\Utility\*.cpp ..\test\UnitTests\Utility\*.cpp ..\src\State\*.cpp ..\test\UnitTests\State\*.cpp ..\src\GUI\*.cpp ..\test\UnitTests\GUI\*.cpp ..\src\Gameplay\*.cpp /I ..\include /link /libpath ..\lib\SDL2.lib
 Tests.exe
 del Tests.exe
 popd
