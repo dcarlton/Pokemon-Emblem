44595ca0547e947e86e535c8b50ee8db2f6e968e
Dustin Carlton <dtc0004@uah.edu>
2015-12-20 14:58:20 -0600

    Added a test for Size's public API

--- test/UnitTests/Utility/TestSize.cpp
+++ test/UnitTests/Utility/TestSize.cpp
@@ -4,6 +4,13 @@
 
 TEST_CASE("Testing the Size utility class")
 {
+    SECTION("Test the basic public API")
+    {
+        Utility::Size size(1, 1);
+        REQUIRE(size.height == 1);
+        REQUIRE(size.width == 1);
+    }
+
     SECTION("Testing converting to a string")
     {
         Utility::Size size(1, 1);
