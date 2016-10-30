9c351185b3e4e5d1d9219841cf9bd77caa50c042
Dustin Carlton <dustin.carlton.is@gmail.com>
2016-10-29 23:48:21 -0500

    Pokemon Emblem now spins up as a process called 'PokemonEmblem'.

--- PokemonEmblem.bat
+++ PokemonEmblem.bat
@@ -2,6 +2,7 @@
 
 pushd build
 cl /EHsc /ZI /MP /W4 /WX /wd4996 /wd4267 ..\Main.cpp ..\src\Gameplay\*.cpp ..\src\Audio\*.cpp ..\src\GUI\*.cpp ..\src\Utility\*.cpp ..\src\State\*.cpp ..\src\State\Controller\*.cpp /I ..\include /link /libpath ..\lib\SDL2.lib /libpath ..\lib\SDL2_ttf.lib /libpath ..\lib\SDL2_mixer.lib
-Main.exe
-del Main.exe
+ren Main.exe PokemonEmblem.exe
+PokemonEmblem.exe
+del PokemonEmblem.exe
 popd
