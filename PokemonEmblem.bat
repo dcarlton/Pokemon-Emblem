b0db39da7d9767e6767ceb9cf9c1969c01ca3f43
Dustin Carlton <dustin.carlton.is@gmail.com>
2017-03-02 23:25:54 -0600

    Fixed everything on Windows.

--- PokemonEmblem.bat
+++ PokemonEmblem.bat
@@ -4,6 +4,6 @@ pushd build
 cl /EHsc /ZI /MP /W4 /WX /wd4996 /wd4267 ..\Main.cpp ..\src\Gameplay\*.cpp ..\src\Audio\*.cpp ..\src\GUI\*.cpp ..\src\Utility\*.cpp ..\src\Filesystem\*.cpp ..\src\State\*.cpp ..\src\State\Controller\*.cpp /I ..\include /link /libpath ..\lib\SDL2.lib /libpath ..\lib\SDL2_ttf.lib /libpath ..\lib\SDL2_mixer.lib
 ren Main.exe PokemonEmblem.exe
 copy PokemonEmblem.exe ..\PokemonEmblem.exe
-..\PokemonEmblem.exe
 del PokemonEmblem.exe
 popd
+PokemonEmblem.exe
