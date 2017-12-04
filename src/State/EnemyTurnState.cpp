c3a84b2b246d7615d279c0a0a13678fbd1f5a7bb
Dustin Carlton <dustin.carlton.is@gmail.com>
2017-12-03 21:57:31 -0600

    Added the framework for the Enemy AI. Next step is implementing the A* algorithm.

--- src/State/EnemyTurnState.cpp
+++ src/State/EnemyTurnState.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 
+#include "../Gameplay/Battle.h"
 #include "EnemyTurnState.h"
 #include "PlayerTurnState.h"
 #include "../Gameplay/Pokemon.h"
@@ -23,17 +24,92 @@ void State::EnemyTurnState::draw()
     _world->drawWorld();
 }
 
+std::vector<State::EnemyTurnState::AttackPlan> State::EnemyTurnState::findAllPossibleAttackPlans(Utility::Point position, std::array<std::shared_ptr<Gameplay::Move>, 4> moves, int numMoves)
+{
+    // TODO: Implement
+    position;
+    moves;
+    numMoves;
+    return std::vector<AttackPlan>();
+}
+
+std::vector<Utility::Point> State::EnemyTurnState::findPathToNearestOpponent(Utility::Point position)
+{
+    // TODO: Implement
+    position;
+    return std::vector<Utility::Point>();
+}
+
+bool State::EnemyTurnState::moveAlongPath(Utility::Point startingPosition, std::vector<Utility::Point> movementPath)
+{
+    Utility::Point currentPosition = startingPosition;
+    for (unsigned int i = 0; i < movementPath.size(); i++)
+    {
+        if (!_world->movePokemon(currentPosition, movementPath[i]))
+        {
+            return false;
+        }
+        currentPosition = movementPath[i];
+    }
+
+    return true;
+}
+
+State::EnemyTurnState::AttackPlan State::EnemyTurnState::pickOptimalAttackPlan(std::shared_ptr<Gameplay::Pokemon> pokemon, std::vector<AttackPlan> attackPlans)
+{
+    // TODO: Implement
+    pokemon;
+    return attackPlans[0];
+}
+
+// Let the enemies take their turn.
+// Needs to be changed when I add animation so the
+// enemy turn doesn't happen instantly.
 void State::EnemyTurnState::update()
 {
     std::vector<Utility::Point> pokemonPositions = _world->getEnemyPokemonPositions();   
     for (uint32 i = 0; i < pokemonPositions.size(); i++)
     {
-        if (!_world->getPokemonFromPosition(pokemonPositions[i])->hasMoved)
+        Utility::Point position = pokemonPositions[i];
+        std::shared_ptr<Gameplay::Pokemon> pokemon = _world->getPokemonFromPosition(position);
+
+        // This boolean should never fail.
+        if (!pokemon->hasMoved)
         {
-            _world->movePokemon(pokemonPositions[i], Utility::Point(pokemonPositions[i].x + 1, pokemonPositions[i].y));
+            std::vector<AttackPlan> possibleAttackPlans = findAllPossibleAttackPlans(position, pokemon->moves, pokemon->getNumMoves());
+            if (!possibleAttackPlans.empty())
+            {
+                AttackPlan optimalAttackPlan = pickOptimalAttackPlan(pokemon, possibleAttackPlans);
+                if (moveAlongPath(position, optimalAttackPlan.movementPath))
+                {
+                    Gameplay::fight(_world, optimalAttackPlan.movementPath.back(), optimalAttackPlan.move, optimalAttackPlan.targetPosition);
+                }
+            }
+            else
+            {
+                std::vector<Utility::Point> movementPath = findPathToNearestOpponent(position);
+                moveAlongPath(position, movementPath);
+            }
         }
     }
 
     _world->resetWhetherEnemyPokemonHaveMoved();
     resetState(std::make_shared<PlayerTurnState>(_world));
 }
+
+
+
+/*
+void ThinkingOutLoud()
+{
+    foreach Pokemon
+        Map<Move, Struct<Targets, Vector<Point>>> result = A*(Vector<Moves>);
+        foreach move:
+            foreach target:
+                Calculate (damage done / current health) / accuracy
+                Save that move and target off if it is the max
+
+        Move along the spaces to reach the optimal target
+        Use the move on the target
+}
+*/
--- src/State/EnemyTurnState.h
+++ src/State/EnemyTurnState.h
@@ -2,10 +2,16 @@
 #define ENEMYTURNSTATE_H
 
 
+#include <array>
 #include <memory>
+#include <vector>
 
 #include "State.h"
+// TODO: Forward declare this
 #include "../Gameplay/World.h"
+
+class Gameplay::Move;
+class Gameplay::Pokemon;
 
 
 namespace State
@@ -20,7 +26,19 @@ public:
     void update();
 
 protected:
+    struct AttackPlan
+    {
+        Gameplay::Move &move;
+        std::vector<Utility::Point> movementPath;
+        Utility::Point targetPosition;
+    };
+
     std::shared_ptr<Gameplay::World> _world;
+
+    std::vector<AttackPlan> findAllPossibleAttackPlans(Utility::Point position, std::array<std::shared_ptr<Gameplay::Move>, 4> moves, int numMoves);
+    std::vector<Utility::Point> findPathToNearestOpponent(Utility::Point position);
+    bool moveAlongPath(Utility::Point startingPosition, std::vector<Utility::Point> movementPath);
+    AttackPlan pickOptimalAttackPlan(std::shared_ptr<Gameplay::Pokemon> pokemon, std::vector<AttackPlan> attackPlans);
 };
 
 }
